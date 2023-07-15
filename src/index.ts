ca8553e6dd9e241acaa443deebdf482078d44718
KaygNas <597857074@QQ.COM>
2023-07-15 08:01:33 +0000

    Init a simple chat bot in terminal

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import * as dotenv from "dotenv";
 import { OpenAI } from "langchain";
+import { ConversationChain } from "langchain/chains";
+import { BufferMemory } from "langchain/memory";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
 dotenv.config();
 
 const model = new OpenAI({
   modelName: "gpt-3.5-turbo",
   openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.9,
 });
 
-const res = await model.call(
-  "What's a good idea for an application to build with GPT-3?"
-);
+const memory = new BufferMemory();
+const chain = new ConversationChain({ llm: model, memory });
+const terminal = readline.createInterface({ input, output });
 
-console.log(res);
+let answer = "";
+// eslint-disable-next-line no-constant-condition
+while (true) {
+  const response = await chain.call({ input: answer });
+  answer = await terminal.question(`[GPT] ${response.response}\n`);
+}
